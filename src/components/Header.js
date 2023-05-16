7666da5553aa72580ca4152f1d3853de89f99754
DelioAraujo <delioaraujo@gmail.com>
2023-05-16 09:11:08 -0300

    item 2 completo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  state = {
+    totalExpenses: 0,
+    exchange: 'BRL',
+  };
+
   render() {
+    const { email } = this.props;
+    const { totalExpenses, exchange } = this.state;
     return (
-      <div>Header</div>
+      <>
+        <div data-testid="email-field">{email}</div>
+        <div data-testid="total-field">{totalExpenses}</div>
+        <div data-testid="header-currency-field">{exchange}</div>
+      </>
     );
   }
 }
 
-const mapStateToProps = (globalState) => {
-  console.log(globalState);
-};
+const mapStateToProps = (globalState) => ({
+  email: globalState.user.email,
+});
+
+Header.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+  }),
+}.isRequired;
 
 export default connect(mapStateToProps)(Header);
